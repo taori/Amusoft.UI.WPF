ef7a0ad37605a3b0294bac49ce797f41ca8d8a18
Andreas Müller <taori@users.noreply.github.com>
2019-01-17 00:15:29 +0100

    Dependency removal + refactoring

--- src/Amusoft.UI.WPF/Adorners/AnchorAdornerManager.cs
+++ src/Amusoft.UI.WPF/Adorners/AnchorAdornerManager.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
-using Amusoft.UI.WPF.Notifications;
 
 namespace Amusoft.UI.WPF.Adorners
 {
@@ -12,13 +13,13 @@ namespace Amusoft.UI.WPF.Adorners
 	{
 		private readonly Visual _target;
 
-		private readonly Dictionary<AnchorPosition, ContentPresenter> _presenters = new Dictionary<AnchorPosition, ContentPresenter>();
+		private readonly Dictionary<Position, ContentPresenter> _presenters = new Dictionary<Position, ContentPresenter>();
 
-		private readonly Dictionary<AnchorPosition, AnchoredAdorner> _adorners = new Dictionary<AnchorPosition, AnchoredAdorner>();
+		private readonly Dictionary<Position, AnchoredAdorner> _adorners = new Dictionary<Position, AnchoredAdorner>();
 
-		public ContentPresenter this[AnchorPosition position] => TryGetPresenter(position, out var p) ? p : null;
+		public ContentPresenter this[Position position] => TryGetPresenter(position, out var p) ? p : null;
 
-		public bool TryGetPresenter(AnchorPosition position, out ContentPresenter presenter)
+		public bool TryGetPresenter(Position position, out ContentPresenter presenter)
 		{
 			if (_presenters.TryGetValue(position, out presenter))
 				return true;
